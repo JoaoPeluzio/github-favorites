456137b7733ca1fedab04dda03764c114996a26b
João Victor Peluzio <jvpeluzio@gmail.com>
2024-03-09 19:28:49 -0300

    project upload

---
+++ README.md
@@ -0,0 +1,27 @@
+
+<p align="center"> <img src="" alt="">
+ </p>
+ 
+<p align="center">
+  <img src=".github/p1.png" alt="Demonstração do projeto" width="100%" />
+</p>
+
+## 🖥️ Projeto
+
+O projeto é um estudo de Javascript. 
+
+
+## 🚀 Tecnologias
+
+Esse projeto foi desennvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- Javascript
+- GIT e GITHUB
+
+## 🏷️ Layout
+
+Você pode visuzalizar o layout do projeto através 
+[Desse link](https://www.figma.com/file/NJbA2WZZVxHpiKNmlXWG1y/%5BDesafios-Explorer%5D-GitFav-(Copy)-(Copy)?type=design&node-id=205-951&mode=design&t=37tilIJwKnXD8Kf3-0).
+É necessário ter uma conta no [Figma](https://www.figma.com)
---
+++ js/Favorites.js
@@ -0,0 +1,143 @@
+import { GitHubUser } from "./GithubUser.js" 
+
+//classe que vai conter a logica dos dados
+
+export class Favorites {
+  constructor(root) {
+    this.root = document.querySelector(root)
+    this.load()
+  }
+
+  load() {
+
+    this.entries = JSON.parse(localStorage.getItem
+      ('@github-favorites:')) || []      
+
+  }
+
+  save() {
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+  }
+
+  async add(username) {
+    try {
+
+      const userExists = this.entries.find(entry => entry.login === username)
+
+  
+
+      if(userExists) {
+        throw new Error('Usuario ja cadastrado')
+      }
+
+
+      const user = await GitHubUser.search(username)
+
+        if(user.login === undefined) {
+        throw new Error('Usuario nao encontrado!')
+        }
+
+        this.entries = [user, ...this.entries]
+        this.update()
+        this.save()
+
+      } catch(error) {
+        alert(error.message)
+    }
+  }
+
+
+  delete(user) {
+    const filteredEntries = this.entries
+      .filter(entry => entry.login !== user.login)
+
+      this.entries = filteredEntries
+      this.update()
+      this.save()
+    }
+  }
+
+//classe que vai criar a visualizacao e eventos do HTML
+
+export class FavoritesView extends Favorites {
+  constructor(root) {
+    super(root)
+
+    this.tbody = this.root.querySelector('table tbody')
+
+    this.update()
+    this.onadd()
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input')
+
+      this.add(value)
+    }
+  }
+
+
+  update() {
+    this.removeAllTr()
+
+
+    this.entries.forEach(user => {
+      const row = this.createRow()
+      
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `imagem de ${user.name}`
+      row.querySelector('.user a').href = `https://github.com/${user.login}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('Tem certeza que deseja deletar esssa linha?')
+          if(isOk) {
+            this.delete(user)
+          }
+      }
+
+      this.tbody.append(row)
+    })
+
+  }
+  
+  createRow() {
+    const tr = document.createElement('tr')
+
+    tr.innerHTML = 
+    `
+    <td class="user">
+      <img src="https://github.com/joaopeluzio.png" alt="Imagem de JoaoPeluzio">
+      <a href="https://github.com/joaopeluzio" target="_blank">
+        <p>João Peluzio</p>
+        <span>JoaoPeluzio</span>
+      </a>
+    </td>
+    <td class="repositories">
+      05
+    </td>
+    <td class="followers">
+      9589
+    </td>
+    <td>
+      <button class="remove">&times;</button>
+    </td>
+  `
+
+    return tr
+
+  }
+  
+  removeAllTr() {
+
+    this.tbody.querySelectorAll('tr')
+    .forEach((tr) => {
+      tr.remove()}
+      )}
+
+}
